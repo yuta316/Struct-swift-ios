ae84568c78227cbf1503617525192d69f4b73a26
yuta316 <iy.24.bump.09@gmail.com>
2021-01-23 04:41:40 +0900

    Initial Commit

---
+++ Struct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Struct
+//
+//  Created by 石川裕太 on 2021/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
